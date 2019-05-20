63c51d45ebd2963904c09fd4d783ab35e983262e
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-05-20 11:48:28 +0800

    Add files via upload

--- GMT-10-script-JOc-KKT.sh
+++ GMT-10-script-JOc-KKT.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Purpose: coastline basemap in Mercator oblique projection (here: Kuril-Kamchatka Trench)
-# GMT modules: pscoast, pstext
+# GMT modules: pscoast, pstext, psconvert
+# Step-1. Generate a file
+ps=GMT_JOc_KKT.ps
+# Step-2. Add coastline
 gmt pscoast -R140/38/170/60r -JOc155/50/155/210/6i -P \
     -Bxa4g4f1 -Bya2g2f1 -Df -B+t"Coastline of the Kuril-Kamchatka area" \
     -Gnavajowhite -Wthinnest \
@@ -10,13 +13,16 @@ gmt pscoast -R140/38/170/60r -JOc155/50/155/210/6i -P \
     --MAP_FRAME_WIDTH=0.1c \
     --MAP_TICK_PEN_PRIMARY=thinner,dimgray \
     --FONT_TITLE=14p,Palatino-Roman,black \
-    --FONT_ANNOT_PRIMARY=7p,Helvetica,dimgray \
-    --FONT_LABEL=7p,Helvetica,dimgray \
+    --FONT_ANNOT_PRIMARY=6p,Palatino-Roman,dimgray \
+    --FONT_LABEL=7p,Palatino-Roman,dimgray \
     --MAP_GRID_PEN_PRIMARY=thinnest \
     -TdjBR+w0.4i+l+o0.15i \
     -Lx5.6i/-0.7i+c50+w1000k+l"Scale, km"+f \
     --MAP_TITLE_OFFSET=0.2i \
-    -U -K > GMT_JOc_KKT.ps
-gmt pstext -R0/8.5/0/11 -Jx1i -F+f7p,Helvetica,dimgray+jCB -N -O >> GMT_JOc_KKT.ps << EOF
+    -UBL/-15p/-45p -K > $ps
+# Step-3. Add footnote
+gmt pstext -R0/8.5/0/11 -Jx1i -F+f7p,Helvetica,dimgray+jCB -N -O >> $ps << EOF
 3.5 -0.7 Mercator oblique projection center: 155\232/50\232, pole: 155\232/210\232
 EOF
+# Step-4. Convert to image file using GhostScript (portrait orientation, 720 dpi)
+gmt psconvert GMT_JOc_KKT.ps -A0.2c -E720 -Tj -P -Z
